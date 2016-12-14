b3ea25ed7f001642ab8f1019b2a0455cafe285ff
xzmcwjzs <1911805752@qq.com>
2016-12-14 10:03:46 +0800

    MT20161214

--- MalignantTumorSystem.WebApplication/Controllers/MedicalHistory_CourseRecordController.cs
+++ MalignantTumorSystem.WebApplication/Controllers/MedicalHistory_CourseRecordController.cs
@@ -11,7 +11,33 @@ namespace MalignantTumorSystem.WebApplication.Controllers
         //
         // GET: /MedicalHistory_CourseRecord/
 
+        #region 基本框架页
         public ActionResult Frame()
+        {
+            return View();
+        }
+        public ActionResult Top()
+        {
+            return View();
+        }
+        public ActionResult Body()
+        {
+            return View();
+        }
+        public ActionResult Left()
+        {
+            return View();
+        }
+        #endregion
+
+        //新增页面
+        public ActionResult CourseRecord()
+        {
+            return View();
+        }
+
+        //列表页
+        public ActionResult List()
         {
             return View();
         }

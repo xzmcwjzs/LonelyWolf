3b0179af498ed47cbb903940d439a32dfd01e716
xzmcwjzs <1911805752@qq.com>
2017-01-10 11:21:08 +0800

    vs2015MT20170110

--- MalignantTumorSystem.WebAPI/App_Start/WebApiConfig.cs
+++ MalignantTumorSystem.WebAPI/App_Start/WebApiConfig.cs
@@ -9,20 +9,16 @@ namespace MalignantTumorSystem.WebAPI
     {
         public static void Register(HttpConfiguration config)
         {
+            // Web API 配置和服务
+
+            // Web API 路由
+            config.MapHttpAttributeRoutes();
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-
-            // 取消注释下面的代码行可对具有 IQueryable 或 IQueryable<T> 返回类型的操作启用查询支持。
-            // 若要避免处理意外查询或恶意查询，请使用 QueryableAttribute 上的验证设置来验证传入查询。
-            // 有关详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=279712。
-            //config.EnableQuerySupport();
-
-            // 若要在应用程序中禁用跟踪，请注释掉或删除以下代码行
-            // 有关详细信息，请参阅: http://www.asp.net/web-api
-            config.EnableSystemDiagnosticsTracing();
         }
     }
 }
--- MalignantTumorSystem.WebAPI/Properties/AssemblyInfo.cs
+++ MalignantTumorSystem.WebAPI/Properties/AssemblyInfo.cs
@@ -10,26 +10,26 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MalignantTumorSystem.WebAPI")]
-[assembly: AssemblyCopyright("版权所有(C)  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
-// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的某个类型，
-// 请针对该类型将 ComVisible 特性设置为 true。
+// 对 COM 组件不可见。如果需要
+// 从 COM 访问此程序集中的某个类型，请针对该类型将 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 
-// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("9a16d411-8edc-4161-ab5b-96bad3e8a47a")]
+// 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
+[assembly: Guid("5a031c5f-a2a5-4cdb-bfad-82cf63007802")]
 
 // 程序集的版本信息由下列四个值组成:
 //
 //      主版本
 //      次版本
 //      内部版本号
-//      修订号
+//      修订版本
 //
-// 可以指定所有这些值，也可以使用“修订号”和“内部版本号”的默认值，
-// 方法是按如下所示使用“*”:
+// 可以指定所有值，也可以使用“修订号”和“内部版本号”的默认值，
+// 方法是按如下所示使用 "*":
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
